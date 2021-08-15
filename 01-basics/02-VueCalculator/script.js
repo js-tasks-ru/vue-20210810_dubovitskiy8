49f1426f1f17c53261c668d255f8b4e7f42923a2
Aleksandr Dubovitskiy <adubovickii@magecom.us>
2021-08-15 15:41:46 +0300

    3st task(Calculator).

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,38 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const RootComponent = defineComponent({
+  name: 'Root',
+  data() {
+    return {
+      firstNumber: '',
+      secondNumber: '',
+      mathExpression: '',
+    };
+  },
+  computed: {
+    summary() {
+      if (!this.firstNumber || !this.secondNumber || !this.mathExpression) {
+        return 0;
+      }
+
+      const firstNumber = parseInt(this.firstNumber);
+      const secondNumber = parseInt(this.secondNumber);
+
+      switch (this.mathExpression) {
+        case 'sum':
+          return firstNumber + secondNumber;
+        case 'subtract':
+          return firstNumber - secondNumber;
+        case 'multiply':
+          return firstNumber * secondNumber;
+        case 'divide':
+          return firstNumber / secondNumber;
+        default:
+          return 0;
+      }
+    },
+  },
+});
+
+createApp(RootComponent).mount('#app');
