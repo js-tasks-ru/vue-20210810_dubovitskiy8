13c61e3dc73a34e737e84e7662826a9bf97dba66
Олександр Дубовицький <odubovytskyi@Oleksandrs-MacBook-Pro.local>
2023-05-20 18:45:11 +0300

    Done -04-vue-cli/01-UiFormGroup1/components/UiFormGroup.vue

--- 04-vue-cli/01-UiFormGroup1/components/UiFormGroup.vue
+++ 04-vue-cli/01-UiFormGroup1/components/UiFormGroup.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="form-group">
+  <div :class="{ 'form-group_inline': inline }" class="form-group">
     <!-- form-group_inline -->
-    <label class="form-group__label">label text</label>
-    <!-- CONTENT -->
+    <label v-if="label" class="form-group__label">{{ label }}</label>
+    <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
   name: 'UiFormGroup',
+  props: {
+    inline: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: false,
+    },
+  },
 };
 </script>
 
